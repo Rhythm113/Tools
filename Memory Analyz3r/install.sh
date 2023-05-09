601fff97d962805056dd0a96212be3febdaae318
Rhythm113 <mahbubanamrhythm@gmail.com>
2023-05-09 14:27:43 +0600

    update install

--- Memory Analyz3r/install.sh
+++ Memory Analyz3r/install.sh
@@ -13,8 +13,8 @@ if ! pkg show libelf && ! pkg show ldd; then
 fi
 
 # Download mema3 and mem from URLs
-wget https://raw.githubusercontent.com/Rhythm113/Tools/master/Memory%20Analyz3r/v0.2/mema3 -O $PREFIX/bin/mema3
-wget https://raw.githubusercontent.com/Rhythm113/Tools/master/Memory%20Analyz3r/v0.2/mem -O $PREFIX/bin/mem
+wget https://raw.githubusercontent.com/Rhythm113/Tools/main/Memory%20Analyz3r/v0.2/mema3 -O $PREFIX/bin/mema3
+wget https://raw.githubusercontent.com/Rhythm113/Tools/main/Memory%20Analyz3r/v0.2/mem -O $PREFIX/bin/mem
 chmod +x $PREFIX/bin/mema3
 chmod +x $PREFIX/bin/mem
 
