0890ec711c517b9de878fa9e2a1ac4994216fa93
Rhythm113 <mahbubanamrhythm@gmail.com>
2023-05-12 16:19:32 +0600

    update v0.3

--- Memory Analyz3r/install.sh
+++ Memory Analyz3r/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Update and upgrade packages
-pkg update && pkg upgrade
+pkg update && pkg upgrade -y
 
 if ! command -v wget
 then
     echo "wget is not installed on your system. Installing..."
-    pkg install wget openssl
+    pkg install wget openssl -y
 else
     echo "wget is already installed on your system continue..."
 fi
@@ -14,11 +14,11 @@ fi
 
 #libelf and ldd 
 echo "installing needed dependencies.."
-pkg install libelf ldd 
+pkg install libelf ldd -y
 
 # Download mema3 and mem from URLs
-wget https://raw.githubusercontent.com/Rhythm113/Tools/main/Memory%20Analyz3r/v0.2/mema3 -O $PREFIX/bin/mema3
-wget https://raw.githubusercontent.com/Rhythm113/Tools/main/Memory%20Analyz3r/v0.2/mem -O $PREFIX/bin/mem
+wget https://raw.githubusercontent.com/Rhythm113/Tools/main/Memory%20Analyz3r/v0.3/mema3 -O $PREFIX/bin/mema3
+wget https://raw.githubusercontent.com/Rhythm113/Tools/main/Memory%20Analyz3r/v0.3/mem -O $PREFIX/bin/mem
 chmod +x $PREFIX/bin/mema3
 chmod +x $PREFIX/bin/mem
 
